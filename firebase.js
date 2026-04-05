5778464b1d4c17efd5af3625b09e3944ecbc672f
onen5552471-ctrl <onen5552471@gmail.com>
2026-04-05 02:09:24 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,14 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "AIzaSyBRh-CGTPNYvRKKa187QRucG9pCp8",
   authDomain: "pothole-6b684.firebaseapp.com",
   projectId: "pothole-6b684",
   storageBucket: "pothole-6b684.appspot.com",
   messagingSenderId: "711077283246",
-  appId: "1:711077283246:web:...",
+  appId: "1:711077283246:web:c6147d5e3f0d0f5a",
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const db = getFirestore(app);
