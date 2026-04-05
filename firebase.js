b45e06d5b14c5469115c1dfa7070ee2922bfeb64
onen5552471-ctrl <onen5552471@gmail.com>
2026-04-05 01:28:41 -0400

    add firebase

--- firebase.js
+++ firebase.js
@@ -1,14 +1,21 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "PASTE_HERE",
-  authDomain: "PASTE_HERE",
-  projectId: "PASTE_HERE",
-  storageBucket: "PASTE_HERE",
-  messagingSenderId: "PASTE_HERE",
-  appId: "PASTE_HERE"
+  apiKey: "AIzaSyBRh-CGTPNYyRKKal87QRucG9pCp8cVDcM",
+  authDomain: "pothole-6b684.firebaseapp.com",
+  projectId: "pothole-6b684",
+  storageBucket: "pothole-6b684.firebasestorage.app",
+  messagingSenderId: "711077283246",
+  appId: "1:711077283246:web:c6147d5e3f0d0f5add343f",
+  measurementId: "G-15E94WM26F"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
